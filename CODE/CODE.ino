6c60186377ebb03e2b1c228489d7dfed0cc1fe3e
thinkerrobotics <90499260+thinkerrobotics@users.noreply.github.com>
2023-09-18 18:38:08 +0530

    Add files via upload

---
+++ CODE/CODE.ino
@@ -0,0 +1,26 @@
+/*
+  ESP32 MAC Address printout
+  esp32-mac-address.ino
+  Prints MAC Address to Serial Monitor
+
+*/
+
+// Include WiFi Library
+#include "WiFi.h"
+
+void setup() {
+
+  // Setup Serial Monitor
+  Serial.begin(115200);
+
+  // Put ESP32 into Station mode
+  WiFi.mode(WIFI_MODE_STA);
+
+  // Print MAC Address to Serial monitor
+  Serial.print("MAC Address: ");
+  Serial.println(WiFi.macAddress());
+}
+
+void loop() {
+
+}
